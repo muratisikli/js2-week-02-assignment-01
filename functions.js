a76646ca0ec6ee9c69f3aac16e378df57850435a
muratisikli <muratisikli80@gmail.com>
2022-11-22 15:26:49 +0100

    Update functions.js
    function are uplooaded

--- functions.js
+++ functions.js
@@ -1,14 +1,28 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (p_Array) => {
+  let odd_num = p_Array.filter((element) => element % 2 == 1);
+  return odd_num.length;
+};
 
-const getIndexOfFalseItem = () => {};
+const getIndexOfFalseItem = (p_index) => {
+const false_index = (element) => element == false;
+return p_index.findIndex(false_index);
+};
 
-const filterNumbers = () => {};
+const filterNumbers = (p_filter_number) => {
+const filter_result = p_filter_number.filter((a) => typeof a == "number");
+return filter_result;
+};
 
-const generateEmailAddress = () => {};
+const generateEmailAddress = (element) => {
+let genererate_email=element.map((person)=>{
+  return person.toLowerCase()+"@gmail.com"
+})
+return genererate_email
+};
 
 export {
-  findTheNumberOfOddNumbers,
-  getIndexOfFalseItem,
-  filterNumbers,
-  generateEmailAddress,
+findTheNumberOfOddNumbers,
+getIndexOfFalseItem,
+filterNumbers,
+generateEmailAddress,
 };
